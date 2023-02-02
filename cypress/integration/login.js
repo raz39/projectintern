4fd4e6a68c6402a2b73188eb7d5136962924e600
raz39 <rajeev.lamichhane@globalyhub.com>
2023-02-02 10:48:11 +0545

    add new test case of not register

--- cypress/integration/login.js
+++ cypress/integration/login.js
@@ -1,8 +1,10 @@
 import Login from "../../POM/loginPage_PO";
 import { faker } from "@faker-js/faker";
+import Signup from "../../POM/signUpPage_PO";
 
 describe("Test for login page", () => {
   const login = new Login();
+  const signup = new Signup();
 
   before(function () {
     cy.visitMainPage();
@@ -29,8 +31,32 @@ describe("Test for login page", () => {
       login.clickSubmitBtn().verifyInvalidCredentials();
     });
 
-    it("should validate valid login sucess message", () => {
-      cy.login();
+    it("should validate not registered button is functional and register then login  ", () => {
+      const name = faker.name.firstName();
+      const surName = faker.name.lastName();
+      const password = faker.internet.password();
+      const phone = faker.phone.number("984######");
+      const userName = faker.internet.userName();
+      const ePost = faker.internet.email();
+      cy.get(".btn.btn-link").click();
+      signup
+        .typeFirstName(name)
+        .typeSurName(surName)
+        .typeEpost(ePost)
+        .typeMobile(phone)
+        .typeUserName(userName)
+        .typePassword(password)
+        .typeConfirmPassword(password)
+        .clickOnSignUpButton()
+        .verifySuccessMessage();
+      cy.get("a.nav-link").last().click();
+      login.typeUserName(userName).typePassword(password).clickSubmitBtn();
+      cy.get("h3").should("have.text", "Welcome " + userName);
+      cy.url().should("include", "azurewebsites.net/Dashboard");
+
+      it("should validate valid login sucess message", () => {
+        cy.login();
+      });
     });
   });
 });
